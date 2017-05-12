7623e54e8a7ecf5a515f20ef38c797271a26b4da
TASoftApplications <info@tasoft.ch>
2017-05-12 17:33:42 +0200

    Initial Commit

---
+++ Placeholders/Placeholders.h
@@ -0,0 +1,19 @@
+//
+//  Placeholders.h
+//  Placeholders
+//
+//  Created by Thomas Abplanalp on 12.05.17.
+//  Copyright © 2017 TASoft Applications. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Placeholders.
+FOUNDATION_EXPORT double PlaceholdersVersionNumber;
+
+//! Project version string for Placeholders.
+FOUNDATION_EXPORT const unsigned char PlaceholdersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Placeholders/PublicHeader.h>
+
+
